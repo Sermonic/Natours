f1734d23c2a5228944b87e1cd8ac8aec5214ba14
Vitalik <vitaliyshcherbanych@gmail.com>
2020-02-20 13:34:03 +0200

    Refactoring for MVC

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,12 +3,7 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
-
-// Create a checkBody middleware
-// Check if body contains the name and price property
-// If not, send back 400 (bad request)
-// Add it to the post handler stack
+// router.param('id', tourController.checkID);
 
 router
   .route('/')
