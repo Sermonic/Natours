8b0676984de8d750a9eb19ae15da6338d341d57d
Vitalik <vitaliyshcherbanych@gmail.com>
2020-03-20 10:00:12 +0200

    Geospatial Queries Finding Tours Within Radius

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const Tour = require('./../models/tourModel');
 const factory = require('./../controllers/handlerFactory');
 const catchAsync = require('./../utils/catchAsync');
-// const AppError = require('./../utils/appError');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -90,6 +90,37 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     status: 'success',
     data: {
       plan
+    }
+  });
+});
+
+// '/tours-within/:distance/center/:latlng/unit/:unit'
+// /tours-within/233/center/34.130443,-118.124309/unit/mi
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng',
+        400
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      data: tours
     }
   });
 });
